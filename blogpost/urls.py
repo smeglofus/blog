9c33593753167c17366ca5bf1bf5d063417d6152
smeglofus <smeglofus@gmail.com>
2023-02-11 09:20:43 +0100

    initial commit

---
+++ blogpost/urls.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.contrib import admin
+from posts.views import homepage, post, category_post_list, allposts
+"""blogpost URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', homepage, name = 'homepage'),
+    path('post/<slug>/', post, name = 'post'),
+    path('postlist/<slug>/', category_post_list, name = 'postlist'),
+    path('posts/', allposts, name = 'allposts'),
+]
---
+++ posts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import Post, Category, Author
+
+# Create your views here.
+
+def homepage(request):
+    categories = Category.objects.all()[0:3]
+    featured = Post.objects.filter(featured=True)
+    latest = Post.objects.order_by('-timestamp')[0:3]
+    context= {
+        'object_list': featured,
+        'latest': latest,
+        'categories': categories,
+    }
+    return render(request, 'homepage.html', context)
+
+def post(request,slug):
+    post = Post.objects.get(slug=slug)
+    context = {
+        'post': post,
+    }
+    return render(request, 'post.html', context)
+
+def category_post_list(request, slug):
+    category = Category.objects.get(slug= slug)
+    posts = Post.objects.filter(categories__in=[category])
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'post_list.html', context)
+
+def allposts(request):
+    posts = Post.objects.order_by('-timestamp')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'all_posts.html', context)
