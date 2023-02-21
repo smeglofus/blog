185f739c989b5d784baf4b2dc053e220ec48ac38
smeglofus <smeglofus@gmail.com>
2023-02-21 19:41:55 +0100

    upgrades and first post

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name = 'homepage'),
@@ -27,3 +30,7 @@ urlpatterns = [
     path('posts/', allposts, name = 'allposts'),
     path('search/', search, name = 'search'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
